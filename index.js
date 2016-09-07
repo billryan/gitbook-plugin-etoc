cce2d6c7a352160b3045dcafb75bb84a00b1d1f3
billryan <yuanbin2014@gmail.com>
2016-09-07 20:36:39 +0800

    fix indent

--- index.js
+++ index.js
@@ -2,27 +2,36 @@ var toc = require('markdown-toc');
 var slug = require('github-slugid');
 
 module.exports = {
-    book: {
-      assets: './assets',
-      css: [
-        "plugin.css"
-      ],
-      js: [
-        "plugin.js"
-      ]
+  book: {
+    assets: './assets',
+    css: [
+      "plugin.css"
+    ],
+    js: [
+      "plugin.js"
+    ]
+  },
+  hooks: {
+    "page:before": function (page) {
+      var _maxdepth = this.config.get('pluginsConfig.etoc.maxdepth') || 3;
+      page.content = toc.insert(page.content, {
+        slugify: function (str) {
+          return slug(str);
+        },
+        append: '\n---\n',
+        maxdepth: _maxdepth
+      });
+      if (this.config.get('pluginsConfig.etoc.addClass')) {
+        var className = this.config.get('pluginsConfig.etoc.className') || 'etoc';
+        page.content = page.content + '\n\n\n<script type="text/javascript">var className=\'' + className + '\';</script>';
+      }
+      return page;
     },
-    hooks: {
-        "page:before": function (page) {
-            page.content = toc.insert(page.content, {
-                slugify: function (str) {
-                  return slug(str);
-                }
-            });
-            if (this.options.pluginsConfig.etoc.addClass) {
-                var className = this.options.pluginsConfig.etoc.className || 'etoc';
-                page.content = page.content + '\n\n\n<script type="text/javascript">var className=\'' + className + '\';</script>';
-            }
-            return page;
-        }
+
+    "page": function (page) {
+      page.content = page.content.replace('<!-- toc -->', '<!-- toc --><div id="toc" class="toc">\n');
+      page.content = page.content.replace('<!-- tocstop -->', '\n</div><!-- tocstop -->');
+      return page;
     }
+  }
 };
