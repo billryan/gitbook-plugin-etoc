f1141bada20231f4819c496d5488aa7dab1b50b6
billryan <yuanbin2014@gmail.com>
2016-09-18 17:08:43 +0800

    fix regex and some bugs

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ module.exports = {
   hooks: {
     "page:before": function (page) {
       var _notoc = this.config.get('pluginsConfig.etoc.notoc') || false;
-      var _existstoc = page.content.indexOf('<!-- toc -->') !== -1;
-      var _existsnotoc = page.content.indexOf('<!-- notoc -->') !== -1;
+      var _existstoc = /^\s*<!-- toc -->\s*$/im.test(page.content);
+      var _existsnotoc = /^\s*<!-- notoc -->\s*$/im.test(page.content);
       if (_existsnotoc) return page;
       if (_notoc && (!_existstoc)) return page;
       if (!(_notoc || _existstoc)) {
-        // insert <!-- toc --> before the first h2/h3/h4 element
-        page.content = page.content.replace(/[\r\n]##/, eol + '<!-- toc -->' + eol + '##');
+        // insert <!-- toc --> before the first h2 element
+        page.content = page.content.replace(/^##([^#])/m, eol + '<!-- toc -->' + eol + '##' + '$1');
       }
 
       var _mindepth = this.config.get('pluginsConfig.etoc.mindepth') || 3;
@@ -30,22 +30,29 @@ module.exports = {
         console.error("!!!mindepth should be no more than maxdepth");
         return page;
       }
-      var heading = '#'.repeat(_mindepth);
-      var re = new RegExp("[\r|\n|\r\n]" + heading);
-      if (!page.content.match(re)) return page;
+      var re = new RegExp('^#{' + _mindepth + '}[^#]', 'm');
+      if (!re.test(page.content)) return page;
 
-      page.content = toc.insert(page.content, {
+      // markdown-toc do not pass options to generate func,
+      // we should escape <!-- toc --> not beginning with whitespace
+      page.content = page.content.replace(/^(\S.*)<!-- toc -->(.*)$/m, '$1<!-- rawtoc -->$2');
+      // we do not process <!-- tocstop -->
+      var options = {};
+      options.regex = /^\s*(?:<!-- toc -->)\s*$/im;
+      page.content = toc.insert(page.content, options, {
         slugify: function (str) {
           return slug(str);
         },
         maxdepth: _maxdepth
       });
+      page.content = page.content.replace('<!-- rawtoc -->', '<!-- toc -->');
       return page;
     },
 
     "page": function (page) {
-      page.content = page.content.replace('<!-- toc -->', '<!-- toc --><div id="toc" class="toc">\n');
-      page.content = page.content.replace('<!-- tocstop -->', '\n</div><!-- tocstop -->');
+      // add toc id and class
+      page.content = page.content.replace('<!-- toc -->', '<!-- toc --><div id="toc" class="toc">' + eol);
+      page.content = page.content.replace('<!-- tocstop -->', eol + '</div><!-- tocstop -->');
       return page;
     }
   }
